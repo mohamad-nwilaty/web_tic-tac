0f627718e398d4e03403738c5a7bb2dc317459c9
Mohamad nwilaty <mhmadnwilaty@gmail.com>
2024-09-01 01:26:11 +0300

    finished the game

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ const oClass = "o" ;
 let Xturn = true ;
 let counter = 0 ;
 const board = document.getElementById("board");
-
+const cellElements = document.querySelectorAll('[data-cell]');
+const restartScreen = document.getElementById("Game-over");
+const restartButton = document.getElementById("restart");
+const result = document.getElementById("result");
+const WiningMoves = [
+    [0,1,2] , [3,4,5] , [6,7,8],
+    [0,3,6] , [1,4,7] , [2,5,8],
+    [0,4,8] , [2,4,6]
+]
+restartButton.addEventListener('click' , (e)=>{
+    restartScreen.style.display = "none";
+    startGame();
+})
 
 
 startGame()
@@ -11,16 +23,20 @@ startGame()
 function startGame(){
     counter = 0
 
-    const cellElements = document.querySelectorAll('[data-cell]');
-        cellElements.forEach(cell =>{
-            cell.addEventListener('click' , handleClick , {once : true})
+    cellElements.forEach(cell =>{
+        cell.addEventListener('click' , handleClick , {once : true})
     });
 
     cellElements.forEach(cell  =>{
         cell.classList.remove(oClass);
         cell.classList.remove(xClass);
     })
-    boardPreview(oClass);
+    if(Xturn){
+        boardPreview(oClass);
+    }
+    else{
+        boardPreview(xClass);
+    }
 }
 
 
@@ -28,10 +44,18 @@ function handleClick(e){
     const cell = e.target;
     const currentTurn = Xturn ? xClass : oClass ;
     placeMark(cell , currentTurn);
-
     boardPreview(currentTurn) 
     swapTurns();
     counter ++ ;
+    if(checkWining(currentTurn)){
+        result.innerHTML = `${currentTurn} has won`;
+        restartScreen.style.display = "flex" ;
+    }
+    else if(counter === 9){
+        result.innerHTML = "Draw";
+        restartScreen.style.display = "flex" ;
+    }
+    
     
 }
 
@@ -51,4 +75,11 @@ function boardPreview(turn){
     else{
         board.classList.add(xClass);
     }
+}
+function checkWining(currentTurn){
+    return WiningMoves.some(move =>{
+        return move.every(index =>{
+            return cellElements[index].classList.contains(currentTurn) ;
+        })
+    })
 }
